cad864e2c43b411c24068ea9853adad5b81a2b81
vquine <01010110.v@gmail.com>
2022-02-09 17:13:59 -0500

    allow the API to accept updates to artwork attribute values, create a private method that will allow 'likes' to bemessed around with

--- app/controllers/api/v1/artworks_controller.rb
+++ app/controllers/api/v1/artworks_controller.rb
@@ -11,4 +11,21 @@ class Api::V1::ArtworksController < ApplicationController
         artwork ? (render json: artwork) : (render json: { message: 'aRtWoRk nOt FoUnD XC' })
     end
 
+    def update
+        artwork = Artwork.find_by_id(params[:id])
+        if artwork
+            artwork.update(artwork_params)
+            render json: artwork
+        else
+            render json: { message: 'E R R O R - CANNOT UPDATE - ARTWORK NOT FOUND'}
+        end
+    end
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    private
+        def artwork_params
+            params.require(:artwork).permit( :likes )
+        end
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 end
